128e39a9b0534fdc68aab04ac26600ce0a7edc40
tgray017 <tgray017@gmail.com>
2018-12-07 23:50:07 +0000

    first commit

---
+++ lib/got_character_query.rb
@@ -0,0 +1,6 @@
+require "got_character_query/version"
+
+module GotCharacterQuery
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/got_character_query/version.rb
@@ -0,0 +1,3 @@
+module GotCharacterQuery
+  VERSION = "0.1.0"
+end
