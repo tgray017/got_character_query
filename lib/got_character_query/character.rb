57d864bd168eb090911c716c6d9c6234b1a7359c
tgray017 <tgray017@gmail.com>
2018-12-09 16:25:15 +0000

    Adjusting xpath

--- lib/got_character_query/character.rb
+++ lib/got_character_query/character.rb
@@ -6,7 +6,9 @@ class Character
   
   def initialize
     html = Nokogiri::HTML(open('https://en.wikipedia.org/wiki/List_of_Game_of_Thrones_characters'))
-    adjusted_html = html.xpath('//h2[1]/following-sibling::p') & html.xpath('//h2[2]/preceding-sibling::p')
+    adjusted_html = html.xpath('//h2[2]/following-sibling::h3') & html.xpath('//h2[3]/preceding-sibling::h3')
+    
+    #adjusted_html.each {|thing| puts thing}
     #adjusted_html = html.xpath('//h2/following-sibling::h2') & html.xpath('//h2[last]')
     #adjusted_html = html.xpath('//span[@id="Main_characters"]') & html.xpath('//span[@id="Main_characters"]/following-sibling::h2')
     #& html.xpath('//span[@id="Supporting_characters"]')
