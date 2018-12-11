6105d2f93c5ea8f59ccda88b46b2bd9fd7dcebaa
tgray017 <tgray017@gmail.com>
2018-12-11 12:56:58 +0000

    Finalized Xpath for main characters

--- lib/got_character_query/character.rb
+++ lib/got_character_query/character.rb
@@ -3,13 +3,32 @@ require 'pry'
 require 'nokogiri'
 
 class Character
+  attr_reader :name
+  
+  def scrape_for_characters
+    html = Nokogiri::HTML(open('https://en.wikipedia.org/wiki/List_of_Game_of_Thrones_characters'))
+    main_characters = html.xpath('//h2[span[@id="Main_characters"]]/following-sibling::node()[following-sibling::h2[span[@id="Supporting_characters"]]]')
+    
+    
+    
+    main_characters.each do |x|
+      @name = x.css('.mw-headline').text
+      @link_to_bio = x.css(".hatnote.navigation-not-searchable")
+      
+      binding.pry
+    end
+      
+    
+    
+    #main_characters.each {|thing| puts thing.text}
+
+  end
+  
   
   def initialize
-    html = Nokogiri::HTML(open('https://en.wikipedia.org/wiki/List_of_Game_of_Thrones_characters'))
-    adjusted_html3 = html.xpath('//h2[span[@id="Main_characters"]]/following-sibling::node()[following-sibling::h2[span[@id="Supporting_characters"]]]')
     
-    #adjusted_html3.each {|thing| puts thing.text}
-    binding.pry
+    
+
   end
   
   def characters
@@ -29,4 +48,4 @@ class Character
   
 end
 
-Character.new
+Character.new.scrape_for_characters
