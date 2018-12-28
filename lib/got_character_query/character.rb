851c372176523ecee0e86049c292080ac52e4c3a
tgray017 <tgray017@gmail.com>
2018-12-28 21:07:15 +0000

    Fixed logic for colons in #scrape_for_character_properties

--- lib/got_character_query/character.rb
+++ lib/got_character_query/character.rb
@@ -30,122 +30,50 @@ class Character
     properties = {}
     
     info_box.each_with_index do |element, index|
-      #binding.pry
       unless element.css('th[scope="row"]').empty?
         property = element.css('th[scope="row"]').text.downcase.gsub(/\s+/, "_").gsub("(", "").gsub(")", "").to_sym
         values = []
         info_box[index].css('td').xpath('..//descendant-or-self::li|a | ..//descendant-or-self::td[not(*)]').each {|value| values << value.text}
           #changing from li to node() triples the values for some reason. Need to find a way for td's to be included for td's that are themselves the value. Not sure why the -or-self isn't accomplishing this
           #looks like it's not even assigning the property because it sees it as empty
-        #binding.pry
+
         property_array = []
-        #properties[property] = values
-        #binding.pry
-        
-        values.each_with_index do |value, index|
-          if value.split('').last == ':'
-            subproperties = {}
-            subproperty = value.delete(':').downcase.gsub(/\s+/, "_").gsub("(", "").gsub(")", "").to_sym
-            subvalues = []
-            
-            counter = 1
-            while counter + index < values.size && values[index + counter].split('').last != ':'
-              subvalues << values[index + counter]
-              counter += 1
-            end
-            subproperties[subproperty] = subvalues
-            property_array << subproperties
-            properties[property] = property_array
-          elsif properties.values.last.instance_of?(Array) && properties.values.last.any? {|e| e.is_a?(Hash)} && properties.values.last.collect {|hash| hash.values}.flatten.include?(value)
-            # If the previous entry in the properties hash is an array of hashes and one of the hashes includes a value equal to the current value, do nothing so that the subproperty values aren't included as values in the level above
-          elsif values.any? {|v| v.split("").last == ":"}
-            # do something to mimick the above, but for subproperties that occur towards the bottom (like novels:)
-          else
-            if values.size > 1
-              value_array = []
-              values.each {|value| value_array << value}
-              properties[property] = value_array
-            else
-              properties[property] = value
-            end
-          #elsif properties.values.last.instance_of?(Array) || properties.values.last.instance_of?(String)
-          #elsif properties.values.last.instance_of?(String)
-          #  properties[property] = value
+        index_array = []
+
+        if values.any? {|value| value.split("").last == ":"}
+          values.each_with_index do |v, i| 
+            index_array << i if v.split("").last == ":"
           end
           
-          binding.pry
+          values.each_with_index do |v, i|
+            c = 1
+            subproperties = {}
+            subvalues = []
+            if i < index_array.first
+              property_array << v
+            elsif index_array.include?(i)
+              c1 = 0
+              subproperty = v.delete(':').downcase.gsub(/\s+/, "_").gsub("(", "").gsub(")", "").to_sym
+              
+              while c1 <= index_array.last
+                while (!index_array[c1 + 1].nil? && (i+c) > index_array[c1] && (i + c) < index_array[c1 + 1]) || (i == index_array.last && i + c < values.size)
+                  subvalues << values[i + c]
+                  c += 1
+                end
+                c1 += 1
+              end
+              subproperties[subproperty] = subvalues
+              property_array << subproperties
+            end
+            properties[property] = property_array
+          end
+        else
+          properties[property] = values
         end
       end
     end
+    binding.pry
   end
-
-      
-#        if value.text.split('').last
-#          subproperties = {}
-#          subproperty = value.text.downcase.gsub(/\s+/, "_").gsub(':','').to_sym
-#          subvalues = []
-#          
-#          list_nodes = value.xpath('.//following-sibling::li')
-#          
-#          
-#        
-#        
-#        binding.pry
-#        if value.text.split('').last == ':'
-#          subproperties = {}
-#          subproperty = value.text.downcase.gsub(/\s+/, "_").gsub(':','').to_sym
-#          subvalues = []
-#          
-#          list_nodes = value.xpath('.//following-sibling::li')
-#          
-#          second_subproperties = {}
-#          second_subvalues = []             
-#           
-#            list_nodes.each do |subvalue|
-#              if subvalue.text.split('').last == ':'
-#                second_subproperty = subvalue.text.downcase.gsub(/\s+/, "_").gsub(':','').to_sym
-#                second_list_nodes = subvalue.xpath('.//following-sibling::li')
-#                second_list_nodes.each do |second_subvalue|
-#                  second_subvalues << second_subvalue.text
-#                end
-#                
-#                second_subproperties[second_subproperty] = second_subvalues
-#              else
-#                subvalues << subvalue.text#
-
-#              end
-#              
-#          #  end
-#            end
-#          
-#          subproperties[subproperty] = subvalues
-#          binding.pry
-#        else
-#          
-#          
-#          binding.pry
-#          
-#        end
-          
-          
-          
-
-          #binding.pry
-
-        
-        #info_box[index].css('td').each do |value|
-        #  values << value.text
-        #  binding.pry
-        #end
-        
-        #values.each_with_index do |v, i|
-          #might want a method to turn each value into its own hash if there are multiple values associated with the key
-        #end
-        
-
-        
-        #binding.pry
-      
   
   
   
--- lib/got_character_query/test.rb
+++ lib/got_character_query/test.rb
@@ -3,9 +3,9 @@ require 'pry'
 
 values = 
 ["A Game of Thrones (1996)",
-"Some other thing",
+"Some other thing:",
 "Television",
-"Winter Is Coming (2011):",
+"Winter Is Coming (2011)",
 "Another thing",
 "One last thing",
 "Video game:",
@@ -13,11 +13,11 @@ values =
 "The Lost Lords (2016)",
 "The Lost Lords (2017)",
 "The Lost Lords (2018)",
-"asdf fdffdf:",
-"The Lost Lords (2015)",
-"The Lost Lords (2016)",
-"The Lost Lords (2017)",
-"The Lost Lords (2018)"]
+"asdf fdffdf",
+"The Lost Lords (2019)",
+"The Lost Lords (2020)",
+"The Lost Lords (2021)",
+"The Lost Lords (2022)"]
 
 properties = {}
 property = :first_appearance
@@ -31,86 +31,25 @@ if values.any? {|value| value.split("").last == ":"}
   
   values.each_with_index do |v, i|
     c = 1
-    case
-      # when there is 1 subproperty and its the first one in the values array (index == 0), add it as a subproperty and add everything below it as subvalues 
-      # when there is 1 subproperty and it's not the first one in the values array (index == n), add everything above it as values to the property array, and add all values where the index is between n + 1 and values.size as subvalues to the subproperty where index == n
-      # when there is > 1 subproperty, take the values between the indexes where v.split("").last == ":" evaluates to true and add them as subvalues to the previous index's value until you reach values.size for the last one
-        # if the first index is not at position 0, add everything above it as a value to the property_array
-      when index_array.size == 1 && index_array.include?(0)
-        subproperties = {}
-        subproperty = values[0].delete(':').downcase.gsub(/\s+/, "_").gsub("(", "").gsub(")", "").to_sym
-        subvalues = []
-        
-        while i + c < values.size
+    subproperties = {}
+    subvalues = []
+    if i < index_array.first
+      property_array << v
+    elsif index_array.include?(i)
+      c1 = 0
+      subproperty = v.delete(':').downcase.gsub(/\s+/, "_").gsub("(", "").gsub(")", "").to_sym
+      
+      while c1 <= index_array.last
+        while (!index_array[c1 + 1].nil? && (i+c) > index_array[c1] && (i + c) < index_array[c1 + 1]) || (i == index_array.last && i + c < values.size)
           subvalues << values[i + c]
           c += 1
         end
-        
-        subproperties[subproperty] = subvalues
-        property_array << subproperties
-        properties[property] = property_array
-        
-      when index_array.size == 1 && !index_array.include?(0)
-        subproperties = {}
-        subvalues = []
-        c2 = 0 
-        while c2 < index_array.first
-          property_array << values[c2]
-          c2 += 1
-        end
-        index_array.each do |index|
-          subproperty = values[index].delete(':').downcase.gsub(/\s+/, "_").gsub("(", "").gsub(")", "").to_sym
-          while index + c < values.size
-            subvalues << values[index + c]
-            c += 1
-          end
-          subproperties[subproperty] = subvalues
-          property_array << subproperties
-        end
-        properties[property] = property_array
-        binding.pry
-
-      when index_array.size >1 && index_array.include?(0)
-        subproperties = {}
-        subvalues = []
-        if index_array.include?(i)
-          c1 = 0
-          subproperty = v.delete(':').downcase.gsub(/\s+/, "_").gsub("(", "").gsub(")", "").to_sym
-          
-          while c1 <= index_array.last
-            while (!index_array[c1+1].nil? && (i+c) > index_array[c1] && (i+c) < index_array[c1+1]) || (i == index_array.last && i + c < values.size)
-              subvalues << values[i+c]
-              c += 1
-            end
-            c1 += 1
-          end
-          subproperties[subproperty] = subvalues
-          property_array << subproperties
-        end
-        properties[property] = property_array
-        binding.pry
-        
-      when index_array.size >1 && !index_array.include?(0)
-        subproperties = {}
-        subvalues = []
-        if i < index_array.first
-          property_array << v
-        elsif index_array.include?(i)
-          c1 = 0
-          subproperty = v.delete(':').downcase.gsub(/\s+/, "_").gsub("(", "").gsub(")", "").to_sym
-          
-          while c1 <= index_array.last
-            while (!index_array[c1+1].nil? && (i+c) > index_array[c1] && (i+c) < index_array[c1+1]) || (i == index_array.last && i + c < values.size)
-              subvalues << values[i+c]
-              c += 1
-            end
-            c1 += 1
-          end
-          subproperties[subproperty] = subvalues
-          property_array << subproperties
-        end
-        properties[property] = property_array
-        binding.pry
+        c1 += 1
       end
+      subproperties[subproperty] = subvalues
+      property_array << subproperties
     end
+    properties[property] = property_array
   end
+  binding.pry
+end
